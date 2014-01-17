15bc0a55d3a786756ec37392a02281273a56223e
Michael Thomas <michael.thomas@biscuitlabs.com>
2014-01-16 20:34:24 -0500

    Cleanup Statement & remove more excess fat

--- js/donor.js
+++ js/donor.js
@@ -140,6 +140,81 @@ function loadUI() {
 		});
 		e.preventDefault();
 	});
+
+	// Statement Rendering Handlers
+	$("#donor-statement-button").on("click", function(e) {
+		// Get statement HTML
+		var popup = window.open();
+		$.get('ui/statement.html', function(data) {
+			// Parse HTML into Object
+			var $statement = $($.parseHTML(data));
+			// Now we need to get our statement data
+			$.ajax({
+			  url: 'https://api.giv2giv.org/api/donors/donations.json',
+			  method: 'GET',
+			  data: {"start_date" : "2014-01-01", "end_date" : "2014-12-31" },
+				contentType: "application/json",
+				dataType:"json"}).success(function(response) {		    	 	
+		    	$.each(response.donations, function(k, v) {
+		    		var $row = $statement.find('tbody:last').append('<tr></tr>');		
+						// And now each bit for our row
+						var $col = $statement.find('tr:last').append('<td>' + v.donation.created_at + '</td>');
+						var $col = $statement.find('tr:last').append('<td>Endowment_id: ' + v.donation.endowment_id + '</td>');
+						var $col = $statement.find('tr:last').append('<td>' + v.donation.gross_amount + '</td>');
+					});
+					$statement.find('#statement-total').html("<span>Total:</span>" + response.total);
+					// Open in new Window
+		    	var html = $('<html>').append($statement).html();
+		    	popup.document.write(html);
+		    }).fail(function(data) {
+		    	// Close Window
+		    	popup.close();
+		    	growlError('Opps! There was an error loading your Statement.');
+		    });
+			}).fail(function(data) {
+				// Close Window
+		    popup.close();
+		    growlError('Opps! There was an error loading your Statement.');
+			});
+		e.preventDefault();
+	});
+
+	// Statement Preview (on Date Change)
+	// Not in use
+	// $('#donor_statement_year').on("change", function(e) {
+  	
+ //    /*
+	//   // Get Donor Statement
+	// 	var statement_start_date = new Date($("#donor_statement_year").val(),0,1);
+	// 	var statement_end_date = new Date($("#donor_statement_year").val(),11,31);
+	// 	var pretty_start_date = statement_start_date.getFullYear() + "-" + statement_start_date.getMonth() + "-" + statement_start_date.getDate();
+	// 	var pretty_end_date = statement_end_date.getFullYear() + "-" + statement_end_date.getMonth() + "-" + statement_end_date.getDate();
+	// 	*/
+
+ //  	$.ajax({
+	// 	  url: 'https://api.giv2giv.org/api/donors/donations.json',
+	// 	  method: 'GET',
+	// 	  data: {"start_date" : "2014-01-01", "end_date" : "2014-12-31" },
+	// 		contentType: "application/json",
+	// 		dataType:"json",
+
+	// 	  success: function(response) {
+	// 			// need to do response.donations.each
+	// 			// response looks like: {"donations":[{"donation":{"created_at":"2013-12-01T01:52:34Z","donor_id":6,"endowment_id":11,"gross_amount":333.0,"id":3,"net_amount":323.043,"payment_account_id":2,"shares_added":"0.00323043","transaction_fees":9.957,"updated_at":"2013-12-01T01:52:34Z"}}],"total":333.0}
+	//     		var donations = { date: 'funDate', description: "funDescription", amount: "funAmount" };
+	//     		$(".table-striped tbody").children().remove();
+	//     		$("#donation-row-template").tmpl(donations).appendTo(".table-striped tbody");
+
+	//     	// console.log(data)
+
+	// 	  },
+	// 	  failure: function(response) {
+	// 	  	//console.log(data);
+	// 	  	window.location.href = "index.html";
+	// 	  }
+		
+	// 	});
+	// });
 }
 
 // Stripe Response Handler
