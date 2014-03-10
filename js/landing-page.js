84cf0a59e2cab4ed45a3bf50a6098ad74868cab6
Bill Mei <contact@kortaggio.com>
2014-03-09 22:33:12 -0400

    make draggable object lock to its target upon drop

--- js/landing-page.js
+++ js/landing-page.js
@@ -17,20 +17,22 @@ $(function() {
 		revert: function(event, ui) {
 			$(this).data("uiDraggable").originalPosition = moneyHomePosition;
 			return !event;
-		}
+		},
+		containment: 'document'
 	});
 	$plantPot.droppable({
 		drop: function(event, ui) {
 			$dragPrompt.html('Horray!');
 			moneyHomePosition = {top: 100, left: 550};
-			$beanstalk.grow();
 			$benjamins.remove();
+			$beanstalk.grow();
 		}
 	});
 	$charity.droppable({
 		drop: function(event, ui) {
 			$dragPrompt.html('Donating directly is great, but what if your charity needs more money? Try giving your money to giv2giv instead.');
 			moneyHomePosition = {top: 100, left: 1066};
+			$benjamins.animate(moneyHomePosition)
 		}
 	});
 
