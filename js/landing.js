559628d2ca46048e68d497d1c5cf948d06976068
Kortaggio <contact@kortaggio.com>
2014-06-09 21:28:22 -0400

    new ui for animation

--- js/landing.js
+++ js/landing.js
@@ -12,10 +12,11 @@ $(function() {
 	var $beanstalk = $('#beanstalk');
 	var $charity = $('#charity');
 
+	console.log($benjamins)
 	// Defines where the money goes back to if the user doesn't drop it anywhere
 	var moneyHomePosition = {
-		top: 100,
-		left: 150
+		top: 140,
+		left: 30
 	};
 
 	$benjamins.draggable({
@@ -36,25 +37,25 @@ $(function() {
 	$plantPot.droppable({
 		drop: function(event, ui) {
 			$dragPrompt.html('Awesome :)<br/><span class="secondary-drag-prompt">Your charity will now receive investment income every 90 days, forever.</span>');
-			moneyHomePosition = {top: 100, left: 350};
+			moneyHomePosition = {top: 140, left: 350};
 			$benjamins.animate({
-				top: 100,
+				top: 140,
 				left: 350,
 				opacity: 0
 			},500,function(){$(benjamins).remove();});
 			$beanstalk.grow();
 		},
 		tolerance: 'touch',
-		hoverClass: 'highlight-hover'
+		hoverClass: 'dragover-hover'
 	});
 	$charity.droppable({
 		drop: function(event, ui) {
 			$dragPrompt.html('Donating directly is great, but what if your charity needs more money?<br/><span class="secondary-drag-prompt">Try giving your money to giv2giv instead.</span>');
-			moneyHomePosition = {top: 100, left: 566};
+			moneyHomePosition = {top: 140, left: 566};
 			$benjamins.animate(moneyHomePosition);
 		},
 		tolerance: 'touch',
-		hoverClass: 'highlight-hover'
+		hoverClass: 'dragover-hover'
 	});
 
 	$beanstalk.grow = function() {
