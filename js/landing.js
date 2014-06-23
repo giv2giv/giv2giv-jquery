fedd7b193c5ec5cff01036b9ee7004624d54a62f
Kortaggio <contact@kortaggio.com>
2014-06-22 20:58:10 -0400

    Working money drag animation

--- js/landing.js
+++ js/landing.js
@@ -1,47 +1,48 @@
 /* TODO: 
 - Fill out the Cashflows object to make it rain
-- Improve the art (flat + rounded corners for image sprites)
 - Add passive animation to the wallet to attract attention there
 */
 
 $(function() {
+	var $benjamins;
+	var moneyHomePosition;
 	var $dragPrompt = $('#drag-prompt');
 	var $personWallet = $('#person-wallet');
-	var $benjamins = $('#benjamins');
 	var $flowerpot = $('#flowerpot');
-	// var $flower = $('#flower');
+	var $flower = $('#flower');
 	var $charity = $('#charity');
 
-	// Defines where the money goes back to if the user doesn't drop it anywhere
-	var moneyHomePosition = {
-		top: 140,
-		left: 30
-	};
+	function createNewBenjamins() {
+		// Initialize the benjamins on page load
+		// Create a new dollar bill once the old one disappears
+		$personWallet.before('<div class="interactive-target stationary" id="benjamins"></div>');
+		$benjamins = $('#benjamins');
+		
+		// Defines where the money goes back to if the user doesn't drop it anywhere
+		moneyHomePosition = {
+			top: 108,
+			left: 22,
+			opacity: 0
+		};
 
-	$benjamins.draggable({
-		// From http://stackoverflow.com/a/5848800
-		// Reverts the money to your wallet if you don't drag it into the pot or the charity
-		revert: function(event, ui) {
-			$(this).data("uiDraggable").originalPosition = moneyHomePosition;
-			return !event;
-		},
-		containment: 'document',
-		start: function(event, ui) {
-			$(this).removeClass('stationary');
-		},
-		stop: function(event, ui) {
-			$(this).addClass('stationary');
-		}
-	});
+		$benjamins.draggable({
+			// From http://stackoverflow.com/a/5848800
+			// Reverts the money to your wallet if you don't drag it into the pot or the charity
+			revert: function(event, ui) {
+				disappearIntoPosition(moneyHomePosition);
+				return !event;
+			},
+			containment: 'document',
+			start: function(event, ui) {
+				$(this).removeClass('stationary');
+			}
+		});
+	}
+
 	$flowerpot.droppable({
 		drop: function(event, ui) {
-			$dragPrompt.fadeOut(400, function(){$dragPrompt.remove();});
-			moneyHomePosition = {top: 140, left: 300};
-			$benjamins.animate({
-				top: 140,
-				left: 300,
-				opacity: 0
-			},500,function(){$(benjamins).remove();});
+			moneyHomePosition = {top: 108, left: 300, opacity: 0};
+			disappearIntoPosition(moneyHomePosition);
 			$flower.grow();
 		},
 		tolerance: 'touch',
@@ -49,16 +50,27 @@ $(function() {
 	});
 	$charity.droppable({
 		drop: function(event, ui) {
-			moneyHomePosition = {top: 140, left: 600};
-			$benjamins.animate(moneyHomePosition);
+			moneyHomePosition = {top: 108, left: 600, opacity: 0};
+			disappearIntoPosition(moneyHomePosition);
 		},
 		tolerance: 'touch',
 		hoverClass: 'dragover-hover'
 	});
 
+
 	$flower.grow = function() {
 		$flower.addClass('grown');
 	};
+
+	function disappearIntoPosition(position) {
+		$dragPrompt.fadeOut(400, function(){$dragPrompt.remove();});
+		$benjamins.animate(position,500,function(){
+			$benjamins.remove();
+			createNewBenjamins();
+		});
+	}
+
+	createNewBenjamins();
 
 	function cashflows() {
 		
