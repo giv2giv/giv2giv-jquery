bea333276150579ac24d311f2b313e2654773933
Michael Blinn <michael.blinn@giv2giv.org>
2014-10-01 12:55:56 -0400

    Adding OverlayMapSpider for P.O. box charity markers that overlap

--- js/maps.js
+++ js/maps.js
@@ -7,6 +7,7 @@ var MapsUI = {
 
 // Nampspacing needs love all around
 var map;
+var oms;
 var locations = [];
 
 // Leaflet Icon Path
@@ -21,11 +22,13 @@ function codeAddress(name, address) {
     	var location = new L.latLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
     	locations.push(location);
     	var marker = L.marker(location, {title:name}).addTo(map).bindPopup("<b>"+name+"</b><br>"+address);
+  	  oms.addMarker(marker);  // <-- here
     	var bounds = new L.LatLngBounds(locations);
-		map.fitBounds(bounds);
+			map.fitBounds(bounds);
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
+	console.log(location);
   });
 }
 
@@ -38,12 +41,15 @@ function onStart(charities) {
 	map = new L.Map('map', {center: new L.LatLng(38.44, -78.87), zoom: 9});
 	var googleLayer = new L.Google('ROADMAP');
 	map.addLayer(googleLayer);
+	oms = new OverlappingMarkerSpiderfier(map);
+
 
 	if (charities.length > 0) {
 		for (var i = 0; i < charities.length; i++) {
 			mapAddress = charities[i].charity.address + ', ';
 			mapAddress += charities[i].charity.city + ', ';
 			mapAddress += charities[i].charity.state;
+			console.log(mapAddress);
 			codeAddress(charities[i].charity.name, mapAddress);
 		}
 	}
